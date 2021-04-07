aa7aaba8d95f00fb9e18ea57e8aecf9cea912157
siva4488 <siva4488@users.noreply.github.com>
2021-04-07 23:57:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ provider "azurerm" {
   # More information on the authentication methods supported by
   # the AzureRM Provider can be found here:
   # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
-  # tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "hcmxexample" {
