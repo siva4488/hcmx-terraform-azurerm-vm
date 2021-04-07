90498a0696cce6a3948d21b4c12f72713566ab55
siva4488 <siva4488@users.noreply.github.com>
2021-04-07 23:14:48 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "resource_group_name" {
-  value = "${azurerm_resource_group.default.name}"
+  value = "${azurerm_resource_group.hcmxexample.name}"
 }
 output "network_security_group_name" {
-  value = "${azurerm_network_security_group.default.name}"
+  value = "${azurerm_network_security_group.hcmxexample.name}"
 }
