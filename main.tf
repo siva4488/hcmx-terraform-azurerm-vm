e08f6075ef3de072df71684990e1fcde081454a8
siva4488 <siva4488@users.noreply.github.com>
2021-04-28 18:53:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -114,10 +114,10 @@ resource "azurerm_network_interface" "hcmxexample" {
   }
    
   source_image_reference {
-    publisher = var.os[publisher]
-    offer     = var.os[offer]
+    publisher = var.publisher
+    offer     = var.offer
     sku       = var.sku
-    version   = var.os[os_version]
+    version   = var.os_version
   }
 }
 
